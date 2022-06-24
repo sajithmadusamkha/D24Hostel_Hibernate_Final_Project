68ccef43f902cd9dda74354013c316b234a8b8db
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-06-24 12:11:03 +0530

    Refactor RoomDAOImpl

--- src/lk/D24_Hostel/hostelSystem/controller/ManageRoomFormController.java
+++ src/lk/D24_Hostel/hostelSystem/controller/ManageRoomFormController.java
@@ -1,4 +1,20 @@
 package lk.D24_Hostel.hostelSystem.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class ManageRoomFormController {
+    public TextField txtRoomID;
+    public TextField txtRoomType;
+    public TextField txtKeyMoney;
+    public TextField txtRoomQty;
+    public JFXButton btnSave;
+
+    public void btnSavoOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/lk/D24_Hostel/hostelSystem/dao/custom/impl/RoomDAOImpl.java
+++ src/lk/D24_Hostel/hostelSystem/dao/custom/impl/RoomDAOImpl.java
@@ -1,7 +1,10 @@
 package lk.D24_Hostel.hostelSystem.dao.custom.impl;
 
 import lk.D24_Hostel.hostelSystem.dao.custom.RoomDAO;
+import lk.D24_Hostel.hostelSystem.db.HibernateUtil;
 import lk.D24_Hostel.hostelSystem.entity.Room;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -9,17 +12,39 @@ public class RoomDAOImpl implements RoomDAO {
 
     @Override
     public boolean add(Room entity) throws Exception {
-        return false;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.save(entity);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public boolean update(Room entity) throws Exception {
-        return false;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(entity);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public boolean delete(String s) throws Exception {
-        return false;
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Room room = session.load(Room.class, s);
+        session.delete(room);
+
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
