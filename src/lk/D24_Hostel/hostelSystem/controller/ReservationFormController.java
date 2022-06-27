3f33641d7b98caf5d06b44dbbc12ec6f60e7a087
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-06-27 16:30:03 +0530

    content updated

--- src/lk/D24_Hostel/hostelSystem/controller/ReservationFormController.java
+++ src/lk/D24_Hostel/hostelSystem/controller/ReservationFormController.java
@@ -1,4 +1,29 @@
 package lk.D24_Hostel.hostelSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ReservationFormController {
+    public ComboBox cmbStudentID;
+    public ComboBox cmbRoomID;
+    public TextField txtReserveId;
+    public TextField txtStudentName;
+    public TextField txtRoomType;
+    public TextField txtKeyMoney;
+    public TextField txtRoomQty;
+    public TextField txtStatus;
+    public TextField txtStudentQty;
+    public TableView tblReservation;
+
+    public void initialize(){
+
+    }
+
+    public void btnReserveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
 }
---
+++ src/lk/D24_Hostel/hostelSystem/view/tdm/ReservationTM.java
@@ -0,0 +1,4 @@
+package lk.D24_Hostel.hostelSystem.view.tdm;
+
+public class ReservationTM {
+}
