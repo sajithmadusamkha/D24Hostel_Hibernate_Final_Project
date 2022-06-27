9183b497c0d4b6ef481ee8c0afb1918af348424f
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-06-27 09:32:50 +0530

    content updated

---
+++ src/lk/D24_Hostel/hostelSystem/dto/ReservationDTO.java
@@ -0,0 +1,4 @@
+package lk.D24_Hostel.hostelSystem.dto;
+
+public class ReservationDTO {
+}
---
+++ src/lk/D24_Hostel/hostelSystem/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package lk.D24_Hostel.hostelSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String userId;
+    private String userName;
+    private String password;
+}
