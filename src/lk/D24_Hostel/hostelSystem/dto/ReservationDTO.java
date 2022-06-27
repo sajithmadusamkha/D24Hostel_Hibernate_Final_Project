9dafe09280bf5995c401a375785d710d6d888769
sajithmadusamkha <sajithmadusankha02@gmail.com>
2022-06-27 11:49:13 +0530

    ResevationDTO added

--- src/lk/D24_Hostel/hostelSystem/dto/ReservationDTO.java
+++ src/lk/D24_Hostel/hostelSystem/dto/ReservationDTO.java
@@ -1,4 +1,22 @@
 package lk.D24_Hostel.hostelSystem.dto;
 
+import lk.D24_Hostel.hostelSystem.entity.Room;
+import lk.D24_Hostel.hostelSystem.entity.Student;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationDTO {
+    private String resId;
+    private LocalDate date;
+    private Student student;
+    private Room room;
+    private double key_money;
+    private String status;
+    private Integer qty;
 }
